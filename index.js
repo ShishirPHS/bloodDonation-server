46f3907a5cc14f38c786579d3f2ce79fe2302f4a
Shishir-Ahmed <shishirahmed790@gmail.com>
2024-03-23 12:29:40 +0600

    created payment intent

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -297,6 +298,26 @@ async function run() {
       } catch (err) {
         res.status(500).json({ message: err.message });
       }
+    });
+
+    // --------------------------------------------------------------------------
+    //                            payment related api
+    // --------------------------------------------------------------------------
+
+    // payment intent
+    app.post("/create-payment-intent", async (req, res) => {
+      const { money } = req.body;
+      const amount = parseInt(money * 100);
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
     });
 
     // Send a ping to confirm a successful connection
